349776e016e07e8c86202cbfd75b749f0dc2d715
jiahui.ying <jiahui.ying@preh.cn>
2018-10-10 10:52:53 +0800

    add jquery dialog and image picker

--- photo/views.py
+++ photo/views.py
@@ -54,6 +54,7 @@ class GalleryView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.kwargs['title']
+        context['photos'] = Photo.objects.exclude(galleries__title = self.kwargs['title'])
 
         return context
 
