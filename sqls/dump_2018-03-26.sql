18d7b03f122eb63823054821787ce7126deb0935
ben <benying1988@gmail.com>
2018-11-16 16:39:27 +0800

    add sqls

---
+++ sqls/dump_2018-03-26.sql
@@ -0,0 +1,5 @@
+
+COPY iaer_iaer (id, money, category, money_type, remark, created, type, chart_type, format, datetime, description, timing, user_id) FROM stdin;
+32	-55	餐饮	0	老婆大人零食	2017-10-26 09:03:24.445768+08	0	0	\N	2017-10-26 09:03:24.446111+08	\N	\N	1
+3	-279	其他	0	树莓派3b	2017-10-26 10:03:31.877377+08	0	0	\N	2017-10-26 10:05:28.234898+08	\N	\N	1
+31	-179	其他	0	BroadLink RM pro	2017-10-25 14:10:01.284436+08	0	0	\N	2017-10-26 10:05:36.61402+08	\N	\N	1
