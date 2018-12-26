8877bed7942d0848b4671902dd246a2ee277bde2
ben <benying1988@gmail.com>
2018-12-26 17:09:20 +0800

    add fund setting

--- iaer/serializers/setting.py
+++ iaer/serializers/setting.py
@@ -9,4 +9,4 @@ class SettingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Setting
-        fields = ['setting_id', 'user_id', 'home_show_current', 'home_show_this_month', 'home_show_this_year', 'created', 'modified']
+        fields = ['setting_id', 'user_id', 'home_show_current', 'home_show_this_month', 'home_show_this_year', 'monthly_fund', 'yearly_fund', 'created', 'modified']
