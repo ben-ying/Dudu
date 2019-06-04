90a2e03ffb4d8109e48158cb47eef04a29e860fb
ben <benying1988@gmail.com>
2019-06-04 21:13:32 +0800

    modify server ip and add sqls

--- myproject/settings.raspberrypi.py
+++ myproject/settings.raspberrypi.py
@@ -2,7 +2,7 @@ from .settings_common import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.201', '192.168.0.201', '192.168.0.200', 'raspberry-server.ddns.net', '107.182.27.191']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.201', '192.168.0.201', '192.168.0.200', 'raspberry-server.ddns.net', '107.182.27.191', '3.13.2.78']
 
 DATABASES = {
     'default': {
