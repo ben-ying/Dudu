2b315f16e07784502478df81f94fd99074f45a78
ben <benying1988@gmail.com>
2019-11-05 15:48:46 +0800

    add ubuntu server

---
+++ myproject/settings.ubuntu.server.py
@@ -0,0 +1,18 @@
+from .settings_common import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['mybackend.xyz', 'www.mybackend.xyz', '120.55.48.57']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
+}
+
+SOURCE_PHOTO_FOLDER = '/root/Pictures'
---
+++ myproject/wsgi.ubuntu.server.py
@@ -0,0 +1,27 @@
+"""
+WSGI config for myproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+project_home = u'/root/app/DjangoApps'
+if project_home not in sys.path:
+    print(project_home)
+    sys.path.append(project_home)
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+application = get_wsgi_application()
+
+from os.path import dirname,abspath
+ 
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR) 
