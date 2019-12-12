cb8cab16b784c2174075dd41ceb278a9815fbcd5
ben <benying1988@gmail.com>
2019-12-12 15:45:41 +0800

    update config

---
+++ apache_config/config.php
@@ -0,0 +1,24 @@
+<--! /var/www/nextcloud/config/config.php -->
+<?php
+$CONFIG = array (
+  'instanceid' => 'oclvzlofwo60',
+  'passwordsalt' => '+gtK7gmimg/TIdh9RubtmES5AwoZfH',
+  'secret' => 'W4ZMeAd3pM0TavHZimkQ9KIQsfMh8a7Cq0Swha6zpkFeU+Zj',
+  'trusted_domains' => 
+  array (
+    0 => 'nextcloud.mybackend.xyz:8088',
+    1 => '60.179.147.83:8001',
+  ),
+  'datadirectory' => '/nas/data/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '17.0.1.1',
+  'overwrite.cli.url' => 'http://nextcloud.mybackend.xyz:8088',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'maben2302',
+  'installed' => true,
+);
